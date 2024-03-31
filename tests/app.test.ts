ccab3ea2ca66255bbd66d9e49017f15ab51a4026
wdavidlopez4 <wdavidlopez4@gmail.com>
2024-03-31 12:58:29 -0500

    tests el app

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,6 +1,26 @@
+import { ServerApp } from "../src/presentation/server-app";
+
 describe('app', () =>{
 
-    test('prueba inicial', () =>{
-        expect(true).toBe(true);
+    test('prueba inicial', async () =>{
+        //hacemos mock de la funcion que llama Server.Run
+        const ServerRunMock = jest.fn();
+        ServerApp.run = ServerRunMock;
+
+        //llamamos al porcess ya que lo esta utilizando
+        process.argv = ['node', 'app.ts', '-b', '10'];
+
+        //hacemos el estimulo (ejecutasmos al app)
+        await import('../src/app');
+
+        //provamos
+        expect(ServerRunMock).toHaveBeenCalledWith({
+            "base": 10, 
+            "destino": 
+            "outputs", 
+            "limit": 10, 
+            "nombre": 
+            "multiplicacion-table", 
+            "showTable": false});
     })
 })
